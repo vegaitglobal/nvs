ed87ae7f7f56f9c243178620b42719dd3062331b
majasveljo <majasveljo@gmail.com>
2019-10-05 22:17:28 +0200

    Partial html css done.

--- customer/my_book.php
+++ customer/my_book.php
@@ -1,139 +1,125 @@
 <center><!-- center Starts -->
 
-    <h1> Volonterska knjižica </h1>
+    <h1>Moja Volonterska knjižica </h1>
 
 </center><!-- center Ends -->
 
-<hr>
+<div class="manifestation-container"><!-- manifestation-container Starts -->
 
-<div class="table-responsive"><!-- table-responsive Starts -->
+    <p>Lista prethodnih manifestacija</p>
 
-    <table class="table table-bordered table-hover"><!-- table table-bordered table-hover Starts -->
+    <div class="table-responsive"><!-- table-responsive Starts -->
 
-        <thead>
+        <table class="table table-bordered table-hover"><!-- table table-bordered table-hover Starts -->
 
-        <tr>
+            <tbody>
 
-            <th> Broj: </th>
-
-            <th> Pozicija </th>
-
-            <th> Sati </th>
-
-            <th> Status </th>
-
-            <th>&nbsp;</th>
-
-        </tr>
-
-        </thead>
-
-        <tbody>
-
-        <?php
+            <?php
 
 
-        $customer_session = $_SESSION['customer_email'];
+            $customer_session = $_SESSION['customer_email'];
 
-        $get_customer = "select * from volunteers where customer_email='$customer_session'";
+            $get_customer = "select * from volunteers where customer_email='$customer_session'";
 
-        $run_customer = mysqli_query($con, $get_customer);
+            $run_customer = mysqli_query($con, $get_customer);
 
-        $row_customer = mysqli_fetch_array($run_customer);
+            $row_customer = mysqli_fetch_array($run_customer);
 
-        $customer_id = $row_customer['customer_id'];
+            $customer_id = $row_customer['customer_id'];
 
-        $i = 0;
+            $i = 0;
 
 
-        $get_wishlist = "select * from wishlist where customer_id='$customer_id' AND status='Prihvaćen'";
+            $get_wishlist = "select * from wishlist where customer_id='$customer_id' AND status='Prihvaćen'";
 
-        $run_wishlist = mysqli_query($con, $get_wishlist);
+            $run_wishlist = mysqli_query($con, $get_wishlist);
 
-        while ($row_wishlist = mysqli_fetch_array($run_wishlist)) {
-            $wishlist_id = $row_wishlist['wishlist_id'];
+            while ($row_wishlist = mysqli_fetch_array($run_wishlist)) {
+                $wishlist_id = $row_wishlist['wishlist_id'];
 
-            $product_id = $row_wishlist['product_id'];
+                $product_id = $row_wishlist['product_id'];
 
-            $hours = $row_wishlist['hours'];
+                $hours = $row_wishlist['hours'];
 
-            $hours_approved = $row_wishlist['hours_approved'];
+                $hours_approved = $row_wishlist['hours_approved'];
 
-            $get_products = "select * from products where product_id='$product_id'";
+                $get_products = "select * from products where product_id='$product_id'";
 
-            $run_products = mysqli_query($con, $get_products);
+                $run_products = mysqli_query($con, $get_products);
 
-            $row_products = mysqli_fetch_array($run_products);
+                $row_products = mysqli_fetch_array($run_products);
 
-            $product_title = $row_products['product_title'];
+                $product_title = $row_products['product_title'];
 
-            $product_url = $row_products['product_url'];
+                $product_url = $row_products['product_url'];
 
-            $product_img1 = $row_products['product_img1'];
+                $product_img1 = $row_products['product_img1'];
 
-            $i++;
+                $i++;
 
-            ?>
+                ?>
 
-            <tr>
+                <tr>
 
-                <td width="100" style="vertical-align:middle" > <?php echo $i; ?> </td>
+                    <td width="100" style="vertical-align:middle"> <span class="manifestation-name"><?php echo $i; ?></span> </td>
 
-                <td style="vertical-align:middle" >
+                    <td style="vertical-align:middle" >
 
-                    <img src="../admin_area/product_images/<?php echo $product_img1; ?>" width="60" height="60">
+                        <img src="../admin_area/product_images/<?php echo $product_img1; ?>" width="60" height="60">
 
-                    &nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;
 
-                    <a href="../pozicija-<?php echo $product_url; ?>">
+                        <a href="../pozicija-<?php echo $product_url; ?>">
 
-                        <?php echo $product_title; ?>
+                            <?php echo $product_title; ?>
 
-                    </a>
-
-                </td>
-
-                <td style="vertical-align:middle" >
-                    <?php  if ($hours==0) : ?>
-                        <a href="index.php?my_book_manage&wishlist_id=<?php echo $wishlist_id ?>">
-                            <i class="fa fa-plus"></i>
-                            Unesi
                         </a>
-                    <?php else : ?>
-                        <?php echo $hours;?>
-                    <?php endif ?>
-                </td>
 
-                <td style="vertical-align:middle" >
-                    <?php if ($hours) : ?>
-                        <?php  if (is_null($hours_approved)) {
-                            echo "U obradi";
-                        } else {
-                            echo $hours_approved ? 'Prihvaćen' : 'Odbijeno';
-                        }?>
-                    <?php endif ?>
-                </td>
+                    </td>
 
-                <td style="vertical-align:middle" >
-                    <?php if ($hours_approved) : ?>
-                        <a class="btn btn-sm btn-default">
-                            <i class="fa fa-download"></i>
-                            PDF
-                        </a>
-                    <?php else : ?>
-                        <a href="index.php?my_book_manage&wishlist_id=<?php echo $wishlist_id ?>">
-                            <i class="fa fa-pencil"></i>
-                            Izmeni
-                        </a>
-                    <?php endif ?>
-                </td>
+                    <td style="vertical-align:middle" >
+                        <?php  if ($hours==0) : ?>
+                            <a href="index.php?my_book_manage&wishlist_id=<?php echo $wishlist_id ?>">
+                                <i class="fa fa-plus"></i>
+                                Unesi
+                            </a>
+                        <?php else : ?>
+                            <?php echo $hours;?>
+                        <?php endif ?>
+                    </td>
 
-            </tr>
+                    <td style="vertical-align:middle" >
+                        <?php if ($hours) : ?>
+                            <?php  if (is_null($hours_approved)) {
+                                echo "U obradi";
+                            } else {
+                                echo $hours_approved ? 'Prihvaćen' : 'Odbijeno';
+                            }?>
+                        <?php endif ?>
+                    </td>
 
-        <?php } ?>
+                    <td style="vertical-align:middle" >
+                        <?php if ($hours_approved) : ?>
+                            <a class="btn btn-sm btn-default">
+                                <i class="fa fa-download"></i>
+                                PDF
+                            </a>
+                        <?php else : ?>
+                            <a href="index.php?my_book_manage&wishlist_id=<?php echo $wishlist_id ?>">
+                                <i class="fa fa-pencil"></i>
+                                Izmeni
+                            </a>
+                        <?php endif ?>
+                    </td>
 
-        </tbody>
+                </tr>
 
-    </table><!-- table table-bordered table-hover Ends -->
+            <?php } ?>
 
-</div><!-- table-responsive Ends -->
+            </tbody>
+
+        </table><!-- table table-bordered table-hover Ends -->
+
+    </div><!-- table-responsive Ends -->
+
+</div><!-- manifestation-container Ends -->
