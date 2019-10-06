c81f180834ef1f03e764bbd8df992308dde35f33
Goran Jankovic <goran.jankovic@vivifyideas.com>
2019-10-06 12:33:38 +0200

    Use doctrine on My Book page, restrict hour entry/editing

--- customer/my_book.php
+++ customer/my_book.php
@@ -14,7 +14,6 @@
 
             <?php
 
-
             $customer_session = $_SESSION['customer_email'];
 
             $get_customer = "select * from volunteers where customer_email='$customer_session'";
@@ -27,31 +26,16 @@
 
             $i = 0;
 
+            $wishlistRepository = $entityManager->getRepository('Wishlist');
+            $wishlists = $wishlistRepository->findBy([
+                'customerId' => $customer_id,
+                'status' => 'Prihvaćen'
+            ]);
 
-            $get_wishlist = "select * from wishlist where customer_id='$customer_id' AND status='Prihvaćen'";
+            $productRepository = $entityManager->getRepository('Product');
 
-            $run_wishlist = mysqli_query($con, $get_wishlist);
-
-            while ($row_wishlist = mysqli_fetch_array($run_wishlist)) {
-                $wishlist_id = $row_wishlist['wishlist_id'];
-
-                $product_id = $row_wishlist['product_id'];
-
-                $hours = $row_wishlist['hours'];
-
-                $hours_approved = $row_wishlist['hours_approved'];
-
-                $get_products = "select * from products where product_id='$product_id'";
-
-                $run_products = mysqli_query($con, $get_products);
-
-                $row_products = mysqli_fetch_array($run_products);
-
-                $product_title = $row_products['product_title'];
-
-                $product_url = $row_products['product_url'];
-
-                $product_img1 = $row_products['product_img1'];
+            foreach ($wishlists as $wishlist) {
+                $product = $productRepository->find($wishlist->getProductId());
 
                 $i++;
 
@@ -63,50 +47,59 @@
 
                     <td style="vertical-align:middle" >
 
-                        <img src="../admin_area/product_images/<?php echo $product_img1; ?>" width="60" height="60">
+                        <img src="../admin_area/product_images/<?php echo $product->getImg1(); ?>" width="60" height="60">
 
                         &nbsp;&nbsp;&nbsp;
 
-                        <a href="../pozicija-<?php echo $product_url; ?>">
+                        <a href="../pozicija-<?php echo $product->getUrl(); ?>">
 
-                            <?php echo $product_title; ?>
+                            <?php echo $product->getTitle(); ?>
 
                         </a>
 
                     </td>
 
+                    <?php
+                        $hours = $wishlist->getHours();
+                        $hours_approved = $wishlist->getHoursApproved();
+                    ?>
+
                     <td style="vertical-align:middle" >
-                        <?php  if ($hours==0) : ?>
-                            <a href="index.php?my_book_manage&wishlist_id=<?php echo $wishlist_id ?>">
+                        <?php  if ($hours == 0 && canEnterHours($wishlist, $product)) : ?>
+                            <a href="index.php?my_book_manage&wishlist_id=<?php echo $wishlist->getId(); ?>">
                                 <i class="fa fa-plus"></i>
                                 Unesi
                             </a>
                         <?php else : ?>
-                            <?php echo $hours;?>
+                            <?php echo $hours; ?>
                         <?php endif ?>
                     </td>
 
                     <td style="vertical-align:middle" >
                         <?php if ($hours) : ?>
                             <?php  if (is_null($hours_approved)) {
-                                echo "U obradi";
+                                echo "Sati u obradi";
                             } else {
-                                echo $hours_approved ? 'Prihvaćen' : 'Odbijeno';
+                                echo $hours_approved ? 'Sati prihvaćeni' : 'Sati odbijeni';
                             }?>
+                        <?php else : ?>
+                            Gotovo
                         <?php endif ?>
                     </td>
 
                     <td style="vertical-align:middle" >
                         <?php if ($hours_approved) : ?>
-                            <a href="wishlist_to_pdf.php?wishlist_id=<?php echo $wishlist_id ?>" class="volunteering-pdf btn btn-sm btn-default">
+                            <a href="wishlist_to_pdf.php?wishlist_id=<?php echo $wishlist->getId(); ?>" class="volunteering-pdf btn btn-sm btn-default">
                                 PDF
                             </a>
-                        <?php else : ?>
+                        <?php elseif (canEnterHours($wishlist, $product)) : ?>
                             <span class="btn btn-link">
                                 <a href="index.php?my_book_manage&wishlist_id=<?php echo $wishlist_id ?>" class="volunteering-pdf btn">
-                                    PDF
+                                    Izmeni
                                 </a>
                             </span>
+                        <?php else : ?>
+                            Gotovo
                         <?php endif ?>
                     </td>
 
@@ -121,3 +114,17 @@
     </div><!-- table-responsive Ends -->
 
 </div><!-- manifestation-container Ends -->
+<?php
+function canEnterHours(Wishlist $wishlist, Product $product)
+{
+    $endDate = $product->getDo();
+    $endDateTwoWeeksLater = $endDate->add(new DateInterval('P2W'));
+    $currentDate = new DateTime('now');
+
+    $dateOk = $currentDate >= $endDate && $currentDate <= $endDateTwoWeeksLater;
+    $accepted = $wishlist->getStatus() === 'Prihvaćen';
+    $hoursApproved = $wishlist->getHoursApproved();
+
+    return $dateOk && $accepted && !$hoursApproved;
+}
+?>
