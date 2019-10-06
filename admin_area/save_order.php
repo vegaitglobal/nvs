a8261427f73d1ffad0e1c985c05c0f45f1fdcdd6
Mladen <mladen.plavsic@gmail.com>
2019-10-06 16:25:44 +0200

    Admin hours

--- admin_area/save_order.php
+++ admin_area/save_order.php
@@ -14,11 +14,10 @@ if (!isset($_SESSION['admin_email'])) {
     if (isset($_POST)) {
         $wishlist = $_POST; 
         $query = "UPDATE wishlist SET hours = ".intval($wishlist['hours']).", hours_approved = ".intval(boolval($wishlist['hours_approved']))." WHERE wishlist_id = ".$wishlist['id']; 
-        var_dump(boolval($_POST['hours_approved']));
         $run = mysqli_query($con, $query);
 
         if ($run) {
-            echo "<script>window.open('index.php?path=view_order','_self')</script>";
+            echo "<script>window.open('index.php?path=view_orders_hours','_self')</script>";
         } else {
             echo "Upit nije prosao";
         }
